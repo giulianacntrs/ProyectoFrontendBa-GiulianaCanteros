2b2e14c9699e6d2bc13e8cf324d965b60e47920e
giulianacntrs <112666134+giulianacntrs@users.noreply.github.com>
2023-11-21 18:14:58 -0300

    Actualizacion repo segunda version

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
     const lista = document.querySelector(".lista");
+    const abrirCarrito = document.querySelector(".carrito");
+    const cerrarCarrito = document.querySelector(".cerrarCarrito");
+    const body = document.querySelector("body");
+    const total = document.querySelector(".total");
+    const cantidad = document.querySelector(".cantidad"); 
+   
+/*     abrirCarrito.addEventListener("click", () => {
+      body.classList.add("active")
+    });
+   
+    cerrarCarrito.addEventListener('click', () => {
+      // Desactivar la interfaz
+      carrito.style.display = 'none';
+      // Desactivar el botón
+      cerrarCarrito.disabled = true;
+      navItem3.style.display = 'block';
+    });
+  */
+
 
 let productos = [
     {
@@ -80,5 +99,61 @@ const IniciarApp = () => {
     })
 }
 IniciarApp();
+
+const agregarAlCarrito = (id) => {
+    // Si el producto ya está en el carrito, incrementa la cantidad
+    let productoEnCarrito = listaCarrito.find(producto => producto.id === id);
+    if (productoEnCarrito) {
+        productoEnCarrito.cantidad++;
+    } else {
+        // Si el producto no está en el carrito, agrega el producto al carrito
+        listaCarrito.push({ ...productos[id], cantidad: 1});
+    }
+    recargarCard(); 
+ }
+ 
+ const recargarCard = () => {
+     listaCarrito = [];
+        let contador = 0;
+        let precioTotal = 0;
+ 
+        listaCarrito.forEach((value, id) => {
+            precioTotal = precioTotal + value.precio;
+            contador = contador + value.cantidad;
+ 
+            if (value != null) {
+               let newDiv = document.createElement("li");
+               newDiv.innerHTML = ` 
+            <div><img src = "img/${value.imagen}"></div>
+            <div class ="tituloCard">${value.nombre}</div>
+            <div class ="precioCard">${value.precio.toLocaleString()}</div>
+ 
+            <div> 
+            <button class="botonCard" onclick ="cambiarCantidad(${id}) 
+            value="${value.cantidad - 1}"></button>
+            <div class= "contador">value="${value.cantidad}</div>
+            <button class="botonCard" onclick ="cambiarCantidad(${id}) 
+            value="${value.cantidad + 1}"></button>
+            </div>
+            `
+            listaCarrito.appendChild(newDiv);
+            }
+            total.innerText = precioTotal.toLocaleString();
+            cantidad.innerText = contador;
+ 
+        })
+    }
+ 
+ const cambiarCantidad = (id, cantidad) => {
+     if (cantidad == 0) {
+         delete listaCarrito[id]
+     }
+     else{
+         listaCarrito[id].cantidad = cantidad;
+         listaCarrito[id].precio = cantidad * productos[id].precio
+     }
+ }
+ 
+ recargarCard();
 })
 
